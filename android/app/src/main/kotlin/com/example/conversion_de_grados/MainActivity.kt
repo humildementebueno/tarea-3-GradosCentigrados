69c8e325c965a6d3ab42295e57cf134e8f856d10
jonatan <themarshial@gmail.com>
2021-08-15 18:12:32 -0400

    version 1

---
+++ android/app/src/main/kotlin/com/example/conversion_de_grados/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.conversion_de_grados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: Principal(),
+    );
+  }
+}
+
+class Principal extends StatefulWidget {
+  @override
+  _PrincipalState createState() => _PrincipalState();
+}
+
+class _PrincipalState extends State<Principal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Conversiones"),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Text(
+                  "Aplicacion Android para la Conversion grados centigrados Fahrenheit")
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
